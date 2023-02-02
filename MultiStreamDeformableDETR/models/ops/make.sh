f4e8971fe2a9a0139a10c5d8efdeee28d71dc132
Minsu Jang <minsu.jang@gmail.com>
2023-02-02 15:39:10 +0900

    Update make.sh

--- MultiStreamDeformableDETR/models/ops/make.sh
+++ MultiStreamDeformableDETR/models/ops/make.sh
@@ -6,5 +6,5 @@
 # ------------------------------------------------------------------------------------------------
 # Modified from https://github.com/chengdazhi/Deformable-Convolution-V2-PyTorch/tree/pytorch_1.0.0
 # ------------------------------------------------------------------------------------------------
-
+export CUDA_HOME=/usr/local/cuda
 python3 setup.py build install
